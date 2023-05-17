81211368863eff40735b011356fd1e2925a44b69
astrobuddha <jozefleo@protonmail.com>
2023-05-16 21:11:53 -0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+
+namespace dotnet_prac // Note: actual namespace depends on the project name.
+{
+    internal class dotnet_prac
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
